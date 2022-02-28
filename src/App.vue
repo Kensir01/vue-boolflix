772db6460a2589da4d08bd5f8dd57b0623f250b2
Demetrio Turchetta <Kensir96@gmail.com>
2022-02-28 17:01:00 +0100

    Push intermedia

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <MyHeader @search='getFilms' />
+    <MyMain />
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+import MyMain from './components/MyMain.vue'
+
+const axios = require('axios');
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    MyMain
+  },
+  data() {
+    return{
+
+    }
+  },
+  methods: {
+    getFilms(keyword) {
+
+      console.log(keyword)
+
+
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=9b07ec6dbfbbc8bbc8e1dc63c15c61b7&query=${keyword}&language=it-IT`)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <form action="">
+            <input type="text" v-model="searchText">
+            <button type="submit" @click.prevent="$emit('search', searchText)" >Ricerca</button>
+        </form>   
+    </header>
+
+  
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+    data() {
+        return {
+            searchText:''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
