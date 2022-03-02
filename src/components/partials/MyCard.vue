7082bfd0fb060a2cfc97880fa43c302ff82c7d74
Demetrio Turchetta <Kensir96@gmail.com>
2022-03-02 16:46:39 +0100

    Carte

--- src/components/partials/MyCard.vue
+++ src/components/partials/MyCard.vue
@@ -1,5 +1,29 @@
 <template>
   <li class="col col-md-6 col-lg-4 col-xl-3 cards overflow">
+      <div class="content" :style="{ backgroundImage: getBackgroundImage(info.poster_path)}">
+          <div class="description">
+              <ul>
+                  <li>
+                      TITOLO: {{getTitle()}}
+                  </li>
+                  <li>
+                      TITOLO ORIGINALE: {{info.original_title}}
+                  </li>
+                  <li>
+                      LINGUA ORIGINALE: <lang-flag :iso="info.original_language"></lang-flag>
+                  </li>
+                  <li>
+                      VOTO:
+                      <i v-for="i in 5" :key="i" class="fa-star" :class="(i < getComputedStar)?'fa-solid':'fa-regular'"></i>
+                  </li>
+                  <li>
+                      TRAMA: {{info.overview}}
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </li>
+  <!-- <li class="col col-md-6 col-lg-4 col-xl-3 cards overflow">
     <div>{{getTitle()}}</div>
     <div>{{info.original_title}}</div>
 
@@ -7,15 +31,14 @@
     <img v-else :src="require('../../assets/img/no_pic.jpg')">
 
     <div>
-        <img v-if="languages.includes(info.original_language)" :src="require('../../assets/flags/'+ info.original_language + '.webp')" >
-        <span v-else>{{info.original_language}}</span>
+        <lang-flag :iso="info.original_language"></lang-flag>
     </div>
     
     <div>
         <i v-for="i in 5" :key="i" class="fa-star" :class="(i < getComputedStar)?'fa-solid':'fa-regular'"></i>
     </div>
 
-  </li>
+  </li> -->
 </template>
 
 <script>
@@ -23,11 +46,6 @@ export default {
     name: 'MyCard',
     props: {
         'info': Object
-    },
-    data () {
-        return {
-            languages: ['en','it'],
-        }
     },
     computed: {
         getComputedStar() {
@@ -42,6 +60,17 @@ export default {
                 return this.info.name;
             }
         },
+        getBackgroundImage() {
+            if (this.info.poster_path) {
+                
+                return `url(https://image.tmdb.org/t/p/w342/${this.info.poster_path})` 
+
+            } else {
+                
+                return `url(https://www.publicdomainpictures.net/pictures/280000/nahled/not-found-image-15383864787lu.jpg)`
+                
+            }
+        }
     }
 }
 </script>
@@ -51,60 +80,59 @@ export default {
 @import "../../assets/style/variabili.scss";
 @import "../../assets/style/generali.scss";
 
+    li {
+        list-style: none;
+    }
+
     .cards {
         height: 300px;
         margin: 20px 0px;
 
-        img {
-            width: 200px;
-            height: 130px;
+
+        .card {
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
         }
 
-        // .card {
-        //     background-size: cover;
-        //     background-repeat: no-repeat;
-        //     background-position: center;
-        // }
+        .content {
+            height: 100%;
+            background-repeat: no-repeat;
+            background-size: contain;
+            background-position: center;
+            border-radius: 5px;
+            overflow-y: auto;
+            position: relative;
 
-        // .content {
-        //     height: 100%;
-        //     background-repeat: no-repeat;
-        //     background-size: cover;
-        //     background-position: center;
-        //     border-radius: 5px;
-        //     overflow-y: auto;
-        //     position: relative;
+            .description {
+                background-color: $dark;
+                font-size: 13px;
+                display: none;
+                min-height: 300px;
+                padding: 10px;
+                position: absolute;
+                left: 0;
+                right: 0;
+                top: 0;
 
-        //     .description {
-        //         background-color: $dark;
-        //         font-size: 13px;
-        //         display: none;
-        //         min-height: 300px;
-        //         padding: 10px;
-        //         position: absolute;
-        //         left: 0;
-        //         right: 0;
-        //         top: 0;
+                ul {
+                    list-style: none;
+                    padding: 0;
 
-        //         ul {
-        //             list-style: none;
-        //             // eliminato padding fastidioso
-        //             padding: 0;
+                    li {
+                    margin: 5px 0px;
+                    text-align: left;
+                    }
+                }
+            }
 
-        //             li {
-        //             margin: 5px 0px;
-        //             text-align: left;
-        //             }
-        //         }
-        //     }
+        }
 
-        // }
-
-        //   .content:hover .description {
-        //         display: block;
-        //         cursor: pointer;
-        //         border: 3px solid $light;
-        //         border-radius: 5px;
-        //     }
+          .content:hover .description {
+                display: block;
+                cursor: pointer;
+                border: 3px solid $light;
+                border-radius: 5px;
+            }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ new Vue({
 }).$mount('#app')
 
 
+import LangFlag from 'vue-lang-code-flags';
+ 
+Vue.component('lang-flag', LangFlag);
+
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -25,20 +29,7 @@ Vue.use(IconsPlugin)
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
-/* import the fontawesome core */
-// import { library } from '@fortawesome/fontawesome-svg-core'
 
-/* import specific icons */
-// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
-/* import font awesome icon component */
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* add icons to the library */
-// library.add(faUserSecret)
-
-/* add font awesome icon component */
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
